fa33d9467dc1cbf87a1f8ae435082611e9fb18ba
Yaroslav Lazarev <y.lazarev@hotmail.com>
2022-01-09 11:28:31 +0000

    Rotational cicher

---
+++ src/test/java/samples/contigous/ContigousSubarraysTest.java
@@ -0,0 +1,23 @@
+package samples.contigous;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ContigousSubarraysTest {
+
+    ContigousSubarrays contigousSubarrays = new ContigousSubarrays();
+
+    @Test
+    void testExample1() {
+        // Given
+        int[] input = new int[] { 3, 4, 1, 6 ,2 };
+        int[] expected = new int[] { 1, 3, 1, 5, 1 };
+
+        // When
+        int[] actual = contigousSubarrays.countSubarrays(input);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+}
