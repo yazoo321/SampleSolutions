360280f28e4c28c54f55473ed3bcd9f83bcd7dbc
Yaroslav Lazarev <y.lazarev@hotmail.com>
2022-01-09 20:23:21 +0000

    add minimum length substrings

---
+++ src/main/java/samples/fb/strings/minimumLengthSubstrings/MinimumLengthSubstrings.java
@@ -0,0 +1,26 @@
+package samples.fb.strings.minimumLengthSubstrings;
+
+// You are given two strings s and t. You can select any substring of string s and rearrange the characters of the selected substring. Determine the minimum length of the substring of s such that string t is a substring of the selected substring.
+// Signature
+// int minLengthSubstring(String s, String t)
+// Input
+// s and t are non-empty strings that contain less than 1,000,000 characters each
+// Output
+// Return the minimum length of the substring of s. If it is not possible, return -1
+// Example
+// s = "dcbefebce"
+// t = "fd"
+// output = 5
+// Explanation:
+// Substring "dcbef" can be rearranged to "cfdeb", "cefdb", and so on. String t is a substring of "cfdeb". Thus, the minimum length required is 5.
+
+
+public class MinimumLengthSubstrings {
+
+    int minLengthSubstring(String s, String t) {
+        // Write your code here
+
+    }
+
+
+}
---
+++ src/test/java/samples/fb/strings/matchingPairs/MatchingPairsTest.java
@@ -0,0 +1,107 @@
+package samples.fb.strings.matchingPairs;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MatchingPairsTest {
+
+    MatchingPairs matchingPairs = new MatchingPairs();
+
+    @Test
+    void testExample1() {
+        // Given
+        String s = "abcd";
+        String t = "adcb";
+        int expected = 4;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testExample2() {
+        // Given
+        String s = "mno";
+        String t = "mno";
+        int expected = 1;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testExample3() {
+        // Given
+        String s = "abcd";
+        String t = "abce";
+        int expected = 2;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testExample4() {
+        // Given
+        String s = "abcde";
+        String t = "adcbe";
+        int expected = 5;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testExample5() {
+        // Given
+        String s = "ab";
+        String t = "cd";
+        int expected = 0;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testExample6() {
+        // Given
+        String s = "abc";
+        String t = "cde";
+        int expected = 1;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testExample7() {
+        // Given
+        String s = "aaa";
+        String t = "aab";
+        int expected = 1;
+
+        // When
+        int actual = matchingPairs.matchingPairs(s, t);
+
+        // Then
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+}
