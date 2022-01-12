249cbe65687c93c0a3bbbd9775e86a6f3e559600
Yaroslav Lazarev <y.lazarev@hotmail.com>
2022-01-12 15:25:53 +0000

    Facebook min length substrings case

--- src/main/java/samples/fb/strings/minimumLengthSubstrings/MinLengthForSubstrings.java
+++ src/main/java/samples/fb/strings/minimumLengthSubstrings/MinLengthForSubstrings.java
@@ -1,8 +1,6 @@
 package samples.fb.strings.minimumLengthSubstrings;
-// Taken from Leetcode sample
-
-// You are given two strings s and t. You can select any substring of string s and rearrange the characters of the selected substring.
-// Determine the minimum length of the substring of s such that string t is a substring of the selected substring.
+// Minimum Length Substrings
+// You are given two strings s and t. You can select any substring of string s and rearrange the characters of the selected substring. Determine the minimum length of the substring of s such that string t is a substring of the selected substring.
 // Signature
 // int minLengthSubstring(String s, String t)
 // Input
@@ -16,14 +14,15 @@ package samples.fb.strings.minimumLengthSubstrings;
 // Explanation:
 // Substring "dcbef" can be rearranged to "cfdeb", "cefdb", and so on. String t is a substring of "cfdeb". Thus, the minimum length required is 5.
 
-
 import java.util.*;
 
-public class MinimumLengthSubstrings {
+public class MinLengthForSubstrings {
 
-    public String minWindow(String s, String t) {
 
-        HashMap<Character, Integer> charMap = new HashMap<>();
+    int minLengthSubstring(String s, String t) {
+        // Write your code here
+
+        Map<Character, Integer> charMap = new HashMap<>();
 
         for (char c : t.toCharArray()) {
             charMap.put(c, charMap.getOrDefault(c, 0) - 1);
@@ -32,34 +31,34 @@ public class MinimumLengthSubstrings {
         int left = 0;
         int right = 0;
         int res = Integer.MAX_VALUE;
-        int start = 0;
 
         for (char c : s.toCharArray()) {
             if (charMap.containsKey(c)) {
                 charMap.put(c, charMap.get(c) + 1);
             }
 
-            // while all values we want are present
-            while(allValuesPresent(charMap) && left <= right) {
+            // if all values are present in this window, try move the left forward
+            while (allCharsPresent(charMap) && left<=right) {
                 int len = right - left + 1;
                 if (len < res) {
                     res = len;
-                    start = left;
                 }
-                char v = s.charAt(left++);
-                if (charMap.containsKey(v)) {
-                    charMap.put(v, charMap.get(v) - 1);
+                char tmp = s.charAt(left++);
+                if (charMap.containsKey(tmp)) {
+                    charMap.put(tmp, charMap.get(tmp) -1);
                 }
+
             }
+
             right++;
         }
 
-        return res == Integer.MAX_VALUE ? "" : s.substring(start, start + res);
+        return res == Integer.MAX_VALUE ? -1 : res;
     }
 
-    boolean allValuesPresent(Map<Character, Integer> charMap) {
-        for (char c : charMap.keySet()) {
-            if (charMap.get(c) < 0) {
+    boolean allCharsPresent(Map<Character, Integer> charMap) {
+        for (char k : charMap.keySet()) {
+            if (charMap.get(k) < 0) {
                 return false;
             }
         }
